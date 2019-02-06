964ba3420d7ab646f91992aac65f65a91b4910aa
jmisiak <julian.misiak@gmail.com>
2019-02-06 12:53:31 -0300

    Primero commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,65 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {AgmCoreModule} from '@agm/core';
+import {ResaltarDirective} from './directives/resaltar.directive';
+import {ContarClicksDirective} from './directives/contar-clicks.directive';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, Routes} from '@angular/router';
+import {DetalleComponent} from './detalle/detalle.component';
+import {LugaresComponent} from './lugares/lugares.component';
+import {ContactoComponent} from './contacto/contacto.component';
+import {LugaresService} from './services/lugares.service';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule, AngularFireDatabase} from '@angular/fire/database';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import { CrearComponent } from './crear/crear.component';
+import {HttpClientModule} from '@angular/common/http';
+
+const appRoutes: Routes = [
+  {path: '', component: LugaresComponent},
+  {path: 'lugares', component: LugaresComponent},
+  {path: 'detalle/:id', component: DetalleComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'crear/:id', component: CrearComponent}
+];
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAMD5wGnK0tBJIiW5ABPxsBISsIRgkYYSo',
+  authDomain: 'platzisquare-b8bb1.firebaseapp.com',
+  databaseURL: 'https://platzisquare-b8bb1.firebaseio.com',
+  storageBucket: 'platzisquare-b8bb1.appspot.com',
+  messagingSenderId: '419520125512'
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective,
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent,
+    ContactoComponent,
+    CrearComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD2kuV6IiDXyCg3hrKJXviZ4bU-_befkWk'
+    }),
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [LugaresService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
